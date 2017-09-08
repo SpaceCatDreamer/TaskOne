4e4471006c5111af56148e2beb77e88fe592724f
SpaceCatDreamer <tanyalittlewhiskey@gmail.com>
2017-09-08 09:54:32 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        System.out.println("Pancakes");
+        /*for (int i = 0; i < 50; i++) {
+            System.out.println(i);
+        }*/
+
+        System.out.println(function(a,b));
+    }
+    public static int function(int a, int b) {
+        return a*b;
+    }
+}
+
