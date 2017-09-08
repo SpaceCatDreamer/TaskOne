53d75867be6779aa354a9d075c84c7fbdbf9b79e
SpaceCatDreamer <tanyalittlewhiskey@gmail.com>
2017-09-08 10:52:11 +0600

    prog works with int nums

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,14 +11,17 @@ public class Main {
         int num1 = in.nextInt();
         int num2 = in.nextInt();
         System.out.println(num1);
+        System.out.println(num2);
+       // float num3 = in.nextFloat();
+       // System.out.println(num3);
         /*for (int i = 0; i < 50; i++) {
             System.out.println(i);
         }
-/*
-        System.out.println(function(a,b));
+*/
+        System.out.println(function(num1,num2));
     }
-    public static int function(int a, int b) {
-        return a*b;
+    public static int function(int num1, int num2) {
+        return num1*num2;
     }
 }
 
