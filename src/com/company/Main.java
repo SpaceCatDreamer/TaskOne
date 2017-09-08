030c6b5e670530e43f9a4c295a27b8f564efb775
SpaceCatDreamer <tanyalittlewhiskey@gmail.com>
2017-09-08 10:34:44 +0600

    ffff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,16 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
         int a = 10;
         int b = 5;
         System.out.println("Pancakes");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите два чила\n");
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        System.out.println(num1);
         /*for (int i = 0; i < 50; i++) {
             System.out.println(i);
         }
